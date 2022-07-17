92448e196b9bffc99f63488fae8e4e5b810850d5
luthfibg <luthfinewzoneonline@gmail.com>
2022-07-17 20:15:00 +0700

    connect to firebase

--- app/src/main/java/com/oxigen/hardware/penstore/FragerStore.java
+++ app/src/main/java/com/oxigen/hardware/penstore/FragerStore.java
@@ -8,12 +8,22 @@ import android.view.ViewGroup;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.zip.Inflater;
 
 public class FragerStore extends Fragment {
+
+    private RecyclerView recyclerView;
+
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_store, container, false);
+
+        View view = inflater.inflate(R.layout.fragment_store, container, false);
+        recyclerView = view.findViewById(R.id.view_recyclerview);
+
+        return view;
     }
 
 }
